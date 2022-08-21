a6aecadaf7e278985e2f9b36340599224cdbc8c7
smartin800 <69486773+smartin800@users.noreply.github.com>
2022-08-21 14:14:48 -0700

    Add files via upload

--- ImageswithFlask.py
+++ ImageswithFlask.py
@@ -81,6 +81,6 @@ def upload_image():
         return render_template('ImageML.html', prediction = 'Invalid File extension')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run()
             
 
